a830774352bfdd8a95d342b071519937581cacd6
AntonioEusebio <A44867@alunos.isel.pt>
2019-09-22 00:23:56 +0100

    tpc_update

--- App.cs
+++ App.cs
@@ -8,11 +8,11 @@ public class App {
 	
     public static bool Foo (String msg)  {
         if(msg.Length == 1) { return true;}
-		
+
 		if(!(msg[0] == msg[msg.Length - 1])) {return false;}
-		
+
 		if(msg.Length ==2) {return true;}
-		
+
 		return Foo(msg.Substring(1,(msg.Length -2)));
     }
 }
